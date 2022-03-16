c6df9f1eab7736f43ea6dd7208bd7b52267a26bf
Вероника Савчук <Veronika-zhigulya@yandex.ua>
2022-03-16 13:42:26 +0300

    comm2

--- homework/Calculator.java
+++ homework/Calculator.java
@@ -9,7 +9,22 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    // BinaryOperator<Integer> devide = (x, y) -> x / y;             изначальный код с ошибкой "деление на 0"
+
+    BinaryOperator<Integer> devide = (x, y) -> y == 0 ? y : x / y;   //с исправлением ошибки
+
+    /*
+   либо:
+
+    BinaryOperator<Integer> devide = (x, y) -> y == 0 ? dev(x, y) : x / y;
+
+    private static Integer dev(Integer x, Integer y) {
+        if(y == 0) {
+            return 0;
+        }
+        return x;
+    }
+    */
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
--- homework/Main.java
+++ homework/Main.java
@@ -13,21 +13,3 @@ public class Main {
         calc.println.accept(c);
     }
 }
-   /*
-   Код не работает, потому что возникает арифметическая ошибка, которая заключается в том, что на 0 делить нельзя.
-   Исправить ошибку можно, если заменить:
-     BinaryOperator<Integer> devide = (x, y) -> x / y;
-   на:
-    BinaryOperator<Integer> devide = (x, y) -> y == 0 ? y : x / y;
-   либо:
-    BinaryOperator<Integer> devide = (x, y) -> y == 0 ? dev(x, y) : x / y;
-
-    private static Integer dev(Integer x, Integer y) {
-        if(y == 0) {
-            return 0;
-        }
-        return x;
-    }
-
-    В обоих случаях при деление на 0 в консоли будет выводиться 0, а исключение ArithmeticException не будет выскакивать.
-    */
